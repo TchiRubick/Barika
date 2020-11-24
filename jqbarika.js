5bfa0bd20aa26e7cc969c67e7c2fdd391e38bba2
Tchi <tchi.devica@gmail.com>
2020-11-24 08:44:25 +0100

    Update jqbarika.js

--- jqbarika.js
+++ jqbarika.js
@@ -1,3 +1,42 @@
+/**
+ * @author Ritchi <tchi.devica@gmail.com> https://github.com/TchiRubick/barika.git
+ * @description State container for Jquery, allow to hide data from DOM but remain available
+ * @name jqBarika : "Barika" in Malagasy means "Baril" in English
+ * 
+ * 
+ * 
+ * 
+ * @tutorial:
+ * 	<script src="jquery.js"></script>
+ *	<script src="jqbarika.js"></script>
+ *	<div id="app"></div>
+ *	<script>
+ *		$( "#app" ).barika( 'setState',
+ *			{
+ *				'stateinitial': 'Nice state',
+ *				'stateFunctional': ( arg ) => console.log( arg )
+ *			}
+ *		);
+ *
+ *		var stateinitial = $( "#app" ).barika( 'getState', 'stateinitial' );
+ *		var stateFunctional = $( "#app" ).barika( 'getState', 'stateFunctional' );
+ *		stateFunctional( stateinitial );
+ *	</script>
+ *
+ * 
+ * 
+ * 
+ * 
+ * @message I'm available to work remotly, contact me.
+ */
+
+
+
+
+
+
+
+
 (function($) {
 	var methods = {
 		getState: function(instance, key) {
@@ -7,8 +46,10 @@
 	};
 
 	$.fn.barika = function(instance, options) {
-		if (instance === "setState") {
-			$(this).data("fname-options", options);
+		var args = {};
+		if (typeof options === "object") {
+			$.extend(args, options);
+			$(this).data("fname-options", args);
 		} else {
 			if (methods[instance]) {
 				return methods[instance].apply(this, arguments);
@@ -16,3 +57,4 @@
 		}
 	};
 })(jQuery);
+
